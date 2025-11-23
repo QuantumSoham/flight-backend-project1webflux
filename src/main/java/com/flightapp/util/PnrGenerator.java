c38567d04766d6dd06434309a304c11562ce618c
QuantumSoham <quantumsc03@gmail.com>
2025-11-23 23:07:34 +0530

    Added Pnr generator

---
+++ src/main/java/com/flightapp/util/PnrGenerator.java
@@ -0,0 +1,26 @@
+package com.flightapp.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+
+@Component
+public class PnrGenerator {
+
+    private static final String ALPHANUM = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private final SecureRandom random = new SecureRandom();
+    private final int length;
+
+    public PnrGenerator(@Value("${app.pnr.length:8}") int length) {
+        this.length = length;
+    }
+
+    public String generate() {
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            sb.append(ALPHANUM.charAt(random.nextInt(ALPHANUM.length())));
+        }
+        return sb.toString();
+    }
+}
