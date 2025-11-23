2a1ef39c8f449f68685dab572a4169927f1bd643
QuantumSoham <quantumsc03@gmail.com>
2025-11-23 21:31:37 +0530

    project initiated

---
+++ src/main/java/com/flightapp/FlightTicketBookingWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.flightapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightTicketBookingWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightTicketBookingWebfluxApplication.class, args);
+	}
+
+}
