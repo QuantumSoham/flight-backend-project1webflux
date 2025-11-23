ab9ef2d14d6dd86e80147afe2ee2c0aa2293839c
QuantumSoham <quantumsc03@gmail.com>
2025-11-23 22:40:11 +0530

    added half repositories

---
+++ src/main/java/com/flightapp/repository/FlightRepository.java
@@ -0,0 +1,14 @@
+package com.flightapp.repository;
+
+import java.time.Instant;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.flightapp.entity.Flight;
+
+import reactor.core.publisher.Flux;
+
+public interface FlightRepository extends ReactiveMongoRepository<Flight,String>
+{
+	Flux<Flight> findByFromPlaceAndToPlaceAndDepartureDateTimeBetween(String from, String to,Instant start,Instant end);
+}
