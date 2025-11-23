49365afe0186a961c2b637a1adda5488ebb602a2
QuantumSoham <quantumsc03@gmail.com>
2025-11-23 22:52:52 +0530

    initialized dto creation

---
+++ src/main/java/com/flightapp/dto/SearchRequest.java
@@ -0,0 +1,14 @@
+package com.flightapp.dto;
+import lombok.Data;
+import java.time.Instant;
+
+//imagine how my request json would look like
+@Data
+public class SearchRequest 
+{
+  private String fromPlace;
+  private String toPlace;
+  private Instant departureDate;
+  private boolean roundTrip;
+  private Instant returnDate;//i am keeping this as a optional param
+}
